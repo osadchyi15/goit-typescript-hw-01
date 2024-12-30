487c5289cdb24308a535f32f392ada764d20d434
Ievgen Osadchyi <osadchyi15@gmail.comi>
2024-12-30 10:54:40 +0100

    ts-hw1

---
+++ src/generics/5.ts
@@ -0,0 +1,15 @@
+// У вас є перелік UserRole, який використовується для класифікації користувачів у вашому додатку. Ви хочете створити об'єкт RoleDescription, який зіставлятиме кожну роль користувача з її описом.
+
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+type RoleDescription = Record<UserRole, string>;
+
+const roleDescription: RoleDescription = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
+};
